8fc5e81d8f985b155ebc300a6e1252c818f5b5a3
moviefan322 <philman202@yahoo.com>
2023-03-09 14:05:20 -0500

    added path for diagnostics

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -1,14 +1,16 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+const diagJSON = require("../db/diagnostics.json");
 
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
+diagnostics.get("/", (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  res.sendFile(diagJSON);
 });
 
 // POST Route for a error logging
-diagnostics.post('/', (req, res) => {
+diagnostics.post("/", (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
+const tipsRouter = require("./tips");
+const feedbackRouter = require("./feedback");
+const diagnosticsRouter = require("./diagnostics");
 // TODO: import your diagnostics route
 
 const app = express();
 
-app.use('/tips', tipsRouter);
-app.use('/feedback', feedbackRouter);
+app.use("/tips", tipsRouter);
+app.use("/feedback", feedbackRouter);
 // TODO: Initialize diagnostics route
+app.use("/diagnostis", diagnosticsRouter);
 
 module.exports = app;
